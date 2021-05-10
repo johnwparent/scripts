2d675f6dfabecb7f2208ece06e54c4e1a1737274
John Parent <john.parent@kitware.com>
2021-05-09 21:45:06 -0400

    add video decomposer

---
+++ im2vid.py
@@ -0,0 +1,60 @@
+import cv2
+import os,sys
+import argparse
+
+video_extensions = [".mp4", ".avi", ".flv", ".gif", ".mov". ".viv", ".svi"]
+
+
+def load_videos(video_directory):
+    loaded_videos = set()
+    if os.path.isdir(video_directory):
+        files = os.scandir()
+        for f in files:
+            _, ext = os.path.splitext(f)
+            if ext in video_extensions:
+                load_videos.add((cv2.VideoCapture(f),f))
+    elif os.path.isfile(video_directory):
+        loaded_videos.add((cv2.VideoCapture(video_directory),video_directory))
+    else:
+        raise RuntimeError("Specified path does not exist or is not a file or directory, please specify a valid filepath")
+
+    if len(loaded_videos):
+        raise RuntimeError("Unable to find or load any video files, please specify a video file or directory containing video files")
+    return loaded_videos
+
+def decompose_video(video, output_dir, video_filename):
+    cont = True
+    name, ext = os.path.splitext(video_filename)
+    name = name.split(os.path.pathsep)[-1]
+    out_name = os.path.join(output_dir,name+"frame%i"+ext)
+    count = 0
+    while cont:
+        cont, frame = video.read()
+        cv2.imwrite(out_name%count,frame)
+        count+=1
+
+def main():
+    args = argparse.ArgumentParser()
+    args.add_argument(
+        "--video-directory",
+        "-v",
+        action="store",
+        dest="video_file",
+        required=True,
+    )
+    args.add_argument(
+        "--output-directory",
+        "-o",
+        dest="output_dir",
+        required=False,
+        default=os.getcwd(),
+        action="store",
+    )
+    opts = args.parse_args(sys.argv[1:])
+    videos = load_videos(opts.video_file)
+    for video in videos:
+        decompose_video(video[0], opts.output_dir, video[1])
+
+
+if __name__ == '__main__':
+    main()
